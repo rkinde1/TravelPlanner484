4f9bfd434ce04c16d033c256192098b00e206b31
Malcolm <malcolm.t129@gmail.com>
2023-03-18 14:14:26 -0400

    learning routing

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('Hello World')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = 5000
+
+const app = express() 
+
+app.get('/get/users',(req,res) => {
+    res.status(200).json({ message: 'Get goals'});
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
