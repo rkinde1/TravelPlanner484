b0d35e671d8995785f1eb7ac86c6edb8592e8068
rkinde1 <rosekinder2020@gmail.com>
2023-03-19 20:06:24 -0400

    Added basic routes for frontend

--- frontend/akihi/src/index.js
+++ frontend/akihi/src/index.js
@@ -10,9 +10,7 @@ import Login from './login.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
     <App />
-    <Date />
   </React.StrictMode>
 );
 
