89dcba5a055631ddcd4eef67cdd5efd3dd67238e
Malcolm <malcolm.t129@gmail.com>
2023-03-25 17:24:11 -0400

    Added login and register capabilities

--- backend/models/loginModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const loginSchema = mongoose.Schema({
-    username: String,
-    password: String
-})
-
-module.exports = mongoose.model('Login', loginSchema);
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.Port || 5000;
 const connectDB = require('./config/db');
+const { getUser, register } = require('./controllers/userController');
 
 
 connectDB()
@@ -10,6 +11,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false}));
 
 app.use('/api/users', require('./routes/userRoute'));
+
+app.use('*', (req, res) => {
+    res.send('404! This not a valid URL.')
+})
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
