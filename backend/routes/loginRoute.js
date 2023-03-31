06f82c2b93a63e55caeb588d61278df4a7e5b814
rkinde1 <rosekinder2020@gmail.com>
2023-03-30 20:21:14 -0400

    Got signup to send data to the backend

--- backend/routes/loginRoute.js
+++ backend/routes/loginRoute.js
@@ -2,4 +2,4 @@ const express = require('express');
 const router = express.Router()
 const { log } = require('../controllers/loginController');
 
-router.route('/login').post(logIn);
+router.route('/signup').post(log);
--- frontend/akihi/src/Signup.js
+++ frontend/akihi/src/Signup.js
@@ -1,85 +1,64 @@
 import React, {useState} from 'react';
 import './Signup.css'
-import {useNavigate} from 'react-router-dom';
+import {Navigate} from 'react-router-dom';
 
 function Signup () {
-    /*
-    const [goToProfile, setGoToProfile] = React.useState(false);
-
-    if (goToProfile){
-        return <Navigate to = "/profile"/>;
-    }
-    */
     const [fname, setFname] = useState("");
     const [lname, setLname] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    let handleSubmit = async (fname, lname, email, password) => {
-            await fetch("/signup", {
+    const [setPosts] = useState([]);
+    let handleSubmit = async (fname, lname, password) => {
+            await fetch("http://localhost:3000/signup", {
                 method: "POST", 
                 body: JSON.stringify({
                     fname : fname,
                     lname : lname,
-                    email : email,
                     password : password
                 }),
-                headers: {
-                    'Content-type' : 'application/json; charset=UTF-8',
-                },
             })
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => {
+                setPosts((posts) => [data, ...posts]);
+                setFname('')
+                setLname('')
+                setPassword('');
+                console.log(data);
+            })
         } 
     return(
-        <div className = "signupForm">
-            < form id = "form" method="POST" action="" onSubmit={handleSubmit}>
+        <div>
+            <form method="POST" onSubmit={handleSubmit}>
                 <fieldset>
                     <h1>Signup</h1>
-                    <label htmlFor="fname">First Name: </label>
-                    <input type="text" id="fname" placeholder="First Name" value={fname} onChange={(e) => setFname(e.target.value)}>
+                    <label>First Name: </label>
+                    <input type="text" id="fname" placeholder="First Name" name="fname" value={fname} onChange={(e) => setFname(e.target.value)}>
                     </input>
                     <br></br>
 
-                    <label htmlFor="lname">Last Name: </label>
-                    <input type="text" id="lname" placeholder="Last Name"
+                    <label for="lname">Last Name: </label>
+                    <input type="text" id="lname" placeholder="Last Name" name="lname"
                         value={lname} onChange={(e) => setLname(e.target.value)}
                         >
                     </input>
                     <br></br>
 
-                    <label htmlFor="email">Email: </label>
-                    <input type="text" id="email" placeholder="Email"
+                    <label for="email">Email: </label>
+                    <input type="text" id="email" placeholder="Email" name="email"
                         value={email} onChange={(e) => setEmail(e.target.value)}
                         >
                     </input>
                     <br></br>
 
-                    <label htmlFor="dob">Date of Birth: </label>
-                    <input type="date" id="dob" placeholder="Date of Birth"
-                       
-                        >
-                    </input>
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" placeholder="Password" name ="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
                     <br></br>
-
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-                    <br></br>
-                    <label htmlFor="verifypassword">Verify Password:</label>
+                    <label for="verifypassword">Verify Password:</label>
                     <input type="password" id="verifypassword" placeholder="Verify Password"></input>
                     <br></br>
-                    <button type="submit" id = "submit" >Sign Up</button>
-                    {/*
-                    onClick={() => {setGoToProfile(true)}}
+                    <button type="submit" id = "submit">Sign Up</button>
                 </fieldset>
-                <p>
-                    Already registered?<br/>
-                    <span className = "line">
-                        {/*router link goes here
-                        <a href = "#">Login</a>
-                    </span>
-                    </p>*/}
-               </fieldset>
-            </form>
+            </form>            
         </div>
 
     );
