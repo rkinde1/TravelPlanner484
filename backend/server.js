06d60e705485f74493d8fd93ddfdc7d258636010
Malcolm <malcolm.t129@gmail.com>
2023-04-06 08:26:09 -0400

    catch up to main

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,13 @@ app.use(express.urlencoded({ extended: false}));
 
 app.use('/api/users', require('./routes/userRoute')); //Dont think we are going to use this route
 
-app.use('/api/signup', require('./routes/signUpRoute')); // |
+app.use('/api/signup', require('./routes/signUpRoute')); // 
 app.use('/api/login', require('./routes/loginRoute')); //  V Both of these will use middleware in the userController
 
-/*
+
 app.use('*', (req, res) => {
     res.send('404! This not a valid URL.')
 })
-*/
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
