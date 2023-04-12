895739d1fab1beff88309197fb7613d6ca5e9d6b
rkinde1 <rosekinder2020@gmail.com>
2023-04-12 12:38:36 -0400

    fixed things

--- frontend/akihi/src/Signup.js
+++ frontend/akihi/src/Signup.js
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 import './Signup.css'
 import {redirect} from 'react-router-dom';
 import {Link } from 'react-router-dom';
-import Axios from 'axios';
 
 function Signup () {
     const [fname, setFname] = useState("");
