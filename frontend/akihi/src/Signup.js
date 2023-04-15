f76d19e0a501da6a34caaca950cbf39a7d4a7a26
Malcolm <malcolm.t129@gmail.com>
2023-04-15 13:10:25 -0400

    added password functions

--- frontend/akihi/src/Signup.js
+++ frontend/akihi/src/Signup.js
@@ -9,6 +9,7 @@ function Signup () {
     const [username, setUsername] = useState("")
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [verifypassword, setVerifyPassword] = useState("");
     let handleSubmit = async (fname, lname, email, username, password) => {
             await fetch("/api/signup", {
                 method: "POST", 
@@ -29,9 +30,37 @@ function Signup () {
             })
     }
 
+    function registerWarden() {
+        
+        if (password.length < 6) {
+            alert("password must be at least 6 characters long");
+            return false;
+        }
+    
+        if (password.length > 24) {
+            alert("password must be less than 24 characters long");
+            return false;
+        }
+        
+        return true;  
+    }
+    
+    
+    function passMatch () {
+    
+        if (password != verifypassword) {
+            window.alert("Ensure passwords match");
+            return false;
+            
+        }
+        else {
+            return true;
+        }
+    } 
+
     return(
         <div className = "screen">
-            <form method="POST" action="/api/signup" onSubmit={handleSubmit} className="form-group form-control no border">
+            <form method="POST" action="/api/signup" onSubmit={registerWarden && passMatch && handleSubmit} className="form-group form-control no border">
                     <div className = "inputContainer">
                     <h1>Signup</h1>
                     <input type="text" id="fname" placeholder="First Name" name="fname" value={fname} onChange={(e) => setFname(e.target.value)}>
@@ -54,7 +83,7 @@ function Signup () {
 
                     <input type="password" id="password" placeholder="Password" name ="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
                     <br></br>
-                    <input type="password" id="verifypassword" placeholder="Verify Password"></input>
+                    <input type="password" id="verifypassword" placeholder="Verify Password" name="verifyPassword" value={verifypassword} onChange={(e) => setVerifyPassword(e.target.value)}></input>
                     <br></br>
                     <button type="submit" className = "hvr-grow btn" id = "submit">Sign Up</button>
                 <br></br>
