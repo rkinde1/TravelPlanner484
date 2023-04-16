3a4465a31ed74b23278cbb6f85b2b779a7f9fa6a
Ibana Smith <ismith9@students.towson.edu>
2023-04-16 05:04:50 -0400

    accidentally put the key in

--- frontend/akihi/src/Components/skyscannerAPISearchbar.js
+++ frontend/akihi/src/Components/skyscannerAPISearchbar.js
@@ -9,8 +9,7 @@ const options = {
   url: 'https://skyscanner-api.p.rapidapi.com/v3/flights/live/search/create',
   headers: {
     'content-type': 'application/json',
-    'X-RapidAPI-Key': '5b83ed9e00mshd25c449e366915cp111505jsn44f812514ba6',
-    'X-RapidAPI-Host': 'skyscanner-api.p.rapidapi.com'
+
   },
   data: '{"query":{"market":"UK","locale":"en-GB","currency":"EUR","queryLegs":[{"originPlaceId":{"iata":"LHR"},"destinationPlaceId":{"iata":"DXB"},"date":{"year":2023,"month":9,"day":20}}],"cabinClass":"CABIN_CLASS_ECONOMY","adults":2,"childrenAges":[3,9]}}'
 };
@@ -29,8 +28,7 @@ const options = {
   method: 'GET',
   url: 'https://skyscanner-api.p.rapidapi.com/v3e/hotels/live/search/poll/1/%7BsessionToken%7D',
   headers: {
-    'X-RapidAPI-Key': '5b83ed9e00mshd25c449e366915cp111505jsn44f812514ba6',
-    'X-RapidAPI-Host': 'skyscanner-api.p.rapidapi.com'
+
   }
 };
 
