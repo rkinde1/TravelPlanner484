7b7195ebd9a7c516b054a8cea868e9d60ec5be71
Ibana Smith <ismith9@students.towson.edu>
2023-04-16 00:24:06 -0400

    Updates on research of APIs that can be used

--- frontend/akihi/src/Reservations.js
+++ frontend/akihi/src/Reservations.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import './App.css'
 
-/*Expedia Affiliate Network API - Expedia Affiliate Network provides APIs that allow developers to build travel booking applications. The APIs offer a range of functionality, including hotel booking, flight booking, and car rental booking. While the APIs do not require a live website, they do require authentication and payment for usage.
-
-Skyscanner Affiliate API - Skyscanner offers an affiliate API that provides flight booking functionality. The API allows developers to build custom flight search and booking applications, and does not require a live website. However, it does require authentication and payment for usage.
-
-Sabre API - Sabre provides a range of travel APIs that offer booking functionality, including flight booking, hotel booking, and car rental booking. While the APIs do not require a live website, they do require authentication and payment for usage.
-
-Kiwi API - Kiwi provides a flight search and booking API that allows developers to build custom flight booking applications. The API does not require a live website, but does require authentication and payment for usage
-
-Rapid API allows 2 free Apis to be tested and mentions
-Skyscanner */
+/*
+Rapid API allows 2 free Apis to be tested and mentions:
+Skyscanner
+ Sabre
+ Booking has some low reviews
+ */
 function Reservations(){
 
 }
