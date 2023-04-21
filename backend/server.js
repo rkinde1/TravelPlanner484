cb3dd324a2acc18b21eaacb9aa250bad0fedf60a
Malcolm <malcolm.t129@gmail.com>
2023-04-21 09:54:56 -0400

    formatting

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,11 @@ app.use(express.json())
 
 app.use('/api/users', require('./routes/userRoute')); //Dont think we are going to use this route
 
-app.use('/api/signup', require('./routes/signUpRoute')); // 
+app.use('/api/signup', require('./routes/signUpRoute')); // Will use middleware in the userController 
 
 
-app.use('/api/login', require('./routes/loginRoute')); // Both of these will use middleware in the userController
+app.use('/api/login', require('./routes/loginRoute')); // Will use middleware in the userController
+
 app.use('/api/itinerary', require('./routes/itineraryRoute'));
 
 app.use('/api/vacation', require('./routes/vacationRoute'));
