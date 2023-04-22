e3c577ce49e2031fa3b2f89af33d8bad9080927f
gberge5 <113952125+gberge5@users.noreply.github.com>
2023-04-22 13:26:19 -0400

    skeleton for budget pie chart

--- frontend/akihi/src/Budget.js
+++ frontend/akihi/src/Budget.js
@@ -1,8 +1,23 @@
+import { Pie Chart } from 'react-minimal-pie-chart';
+
 function Budget(){
     return(
         <div>
             <div>
                 <h2>Plan Your Travel Budget</h2>
+            </div>
+            <div>
+                <br></br>
+                <PieChart
+                    data={[
+                        {title: 'Transport', value: 10, color: '#FF5733'},
+                        {title: 'Hotel', value: 10, color: '#FFC333'},
+                        {title: 'Food', value: 10, color: '#4AEC2F'},
+                        {title: 'Activities', value: 10, color: '#2F64EC'},
+                        {title: 'Shopping', value: 10, color: '#EC2F2F'},
+                    ]}
+                />;
+                <br></br>
             </div>
             <div>
                 <fieldset>
